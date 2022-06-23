760dc577905b6d40cc637e10d43178355f74ddf1
Zeyu Xiong <scyzx2@gmail.com>
2022-06-23 15:46:21 +0800

    Chapter1: Creating and Combining Views

---
+++ SwiftUI_Essentials/Landmarks/Shared/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Shared
+//
+//  Created by BILLXZY1215 on 2022/6/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI_Essentials/README.md
@@ -0,0 +1,3 @@
+# Chapter 1: Swift UI Essentials
+
+## [Creating and Combining Views](https://developer.apple.com/tutorials/swiftui/creating-and-combining-views)
